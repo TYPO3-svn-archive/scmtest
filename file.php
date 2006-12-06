05454d098db79ddc9f0f8a65c2dbe27cba0e3887
webkoch123 <webkoch123@735d13b6-9817-0410-8766-e36946ffe9aa>
2006-12-06 21:11:13 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/scmtest/trunk@4249 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ file.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Created on 04.07.2006
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+ 
+ 
+echo 'Hello World!';
+echo 'Some change';
+echo 'Some new change for tag testing.'; 
+ 
+ 
+?>
