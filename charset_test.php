40716e121467ff713df452004245b1f29528cfd4
flyguide <flyguide@735d13b6-9817-0410-8766-e36946ffe9aa>
2007-05-02 08:37:35 +0000

    utf-8 now
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/scmtest/trunk@5420 735d13b6-9817-0410-8766-e36946ffe9aa

--- charset_test.php
+++ charset_test.php
@@ -2,7 +2,7 @@
 /***************************************************************
 *  Copyright notice
 *
-*  (c) 1999-2005 Kasper Skårhøj (kasperYYYY@typo3.com)
+*  (c) 1999-2005 Kasper SkÃ¥rhÃ¸j (kasperYYYY@typo3.com)
 *  All rights reserved
 *
 *  This script is part of the TYPO3 project. The TYPO3 project is
@@ -25,7 +25,7 @@
 *  This copyright notice MUST APPEAR in all copies of the script!
 ***************************************************************/
 /**
- * @author	Kasper Skårhøj <kasperYYYY@typo3.com>
+ * @author	Kasper SkÃ¥rhÃ¸j <kasperYYYY@typo3.com>
  *
  */
 
